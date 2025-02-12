bfd15c5ab4b8b9f6a112567f8f6fdbee128e48cf
Tebrihk <baremtebrihk@gmail.com>
2025-02-12 16:35:18 +0100

    creating a dash board

---
+++ jamb_dashboard.py
@@ -0,0 +1,19 @@
+from re import template
+from turtle import left
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import streamlit as st
+
+#page header
+st.set_page_config(
+    page_title='Jamb  Analy️sis',
+    page_icon=':bar_chart',
+    layout="wide",
+)
+
+# sidebar
+st.sidebar.title('SIDEBAR CONTROL  :control_knobs:')
+st.title(":bar_chart: JAMB DASHBOARD")
+st.markdown('##')
+
